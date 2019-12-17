91347a5d929c0cf614235b89a15bf1857ca01ca7
Marco <marco.carone@libero.it>
2019-12-17 15:00:54 +0100

    FINE BONUS

---
+++ BONUS/js/script.js
@@ -0,0 +1,13 @@
+function calcola()
+{
+  var primoNumero = parseInt ( prompt ('Inserisci un numero a piacere'));
+  var secondoNumero = parseInt ( prompt ('Inserisci un secondo numero a piacere'));
+  var somma = primoNumero + secondoNumero;
+  var sottr = primoNumero - secondoNumero;
+  var molt = primoNumero * secondoNumero;
+  var div = primoNumero / secondoNumero;
+  alert("La somma è uguale a " + somma);
+  alert("La sottrazione è uguale a " + sottr);
+  alert("La moltiplicazione è uguale a " + molt);
+  alert("La divisione è uguale a " + div);
+}
